f6dc9d42cbad21895a9861dfb5a3f9a7fbda4a00
kvokka <kvokka@yahoo.com>
2019-07-09 01:22:36 +0100

    Simplify test garbage collector setup

--- spec/support/deferred_garbage_collection.rb
+++ spec/support/deferred_garbage_collection.rb
@@ -7,19 +7,19 @@
 class DeferredGarbageCollection
   DEFERRED_GC_THRESHOLD = (ENV["DEFER_GC"] || 15.0).to_f
 
-  @last_gc_run = Time.zone.now
+  @last_gc_run = Time.now
 
   def self.start
     GC.disable
   end
 
   def self.reconsider
-    return unless Time.zone.now - @last_gc_run >= DEFERRED_GC_THRESHOLD
+    return unless Time.now - @last_gc_run >= DEFERRED_GC_THRESHOLD
 
     GC.enable
     GC.start
     GC.disable
-    @last_gc_run = Time.zone.now
+    @last_gc_run = Time.now
   end
 end
 
